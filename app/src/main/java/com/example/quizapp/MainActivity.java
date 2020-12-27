d5a43ca6bdfc0021f349a144084d3fe55a8ea4d4
MasanaIto <z3heng0326@gmail.com>
2020-12-27 23:59:55 +0900

    クイズを管理する配列を作成

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1, answerBtn2, answerBtn3, answerBtn4;
+
+    private String rightAnswer;
+    private int rightAnswerCount;
+    private int quizCount = 1;
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+
+    String[][] quizData = {
+            // {"都道府県名", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"北海道", "札幌市", "長崎市", "福島市", "前橋市"},
+            {"青森県", "青森市", "広島市", "甲府市", "岡山市"},
+            {"岩手県", "盛岡市","大分市", "秋田市", "福岡市"},
+            {"宮城県", "仙台市", "水戸市", "岐阜市", "福井市"},
+            {"秋田県", "秋田市","横浜市", "鳥取市", "仙台市"},
+            {"山形県", "山形市","青森市", "山口市", "奈良市"},
+            {"福島県", "福島市", "盛岡市", "新宿区", "京都市"},
+            {"茨城県", "水戸市", "金沢市", "名古屋市", "奈良市"},
+            {"栃木県", "宇都宮市", "札幌市", "岡山市", "奈良市"},
+            {"群馬県", "前橋市", "福岡市", "松江市", "福井市"},
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countLabel = findViewById(R.id.countLabel);
+        questionLabel = findViewById(R.id.questionLabel);
+        answerBtn1 = findViewById(R.id.answerBtn1);
+        answerBtn2 = findViewById(R.id.answerBtn2);
+        answerBtn3 = findViewById(R.id.answerBtn3);
+        answerBtn4 = findViewById(R.id.answerBtn4);
+
+        // quizDataからクイズ出題用のquizArrayを作成する
+        for (String[] quizDatum : quizData) {
+
+            // 新しいArrayを準備
+            ArrayList<String> tmpArray = new ArrayList<>();
+
+            // クイズデータを追加
+            tmpArray.add(quizDatum[0]); // 都道府県名
+            tmpArray.add(quizDatum[1]); // 正解
+            tmpArray.add(quizDatum[2]); // 選択肢1
+            tmpArray.add(quizDatum[3]); // 選択肢2
+            tmpArray.add(quizDatum[4]); // 選択肢3
+
+            // tmpArrayをquizArrayに追加する
+            quizArray.add(tmpArray);
+        }
     }
 }
